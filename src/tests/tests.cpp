cbe029b2d4d80a35cd60697131738fc5b09290b1
TANK1_41 <bradenaey1@gmail.com>
2022-03-23 15:37:59 -0400

    added const referance to loop

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -16,7 +16,7 @@ namespace test{
 
     }
     void TestMenu::OnImGuiRender() {
-        for (auto test: m_Tests)
+        for (const auto &test: m_Tests)
         {
             if(ImGui::Button(test.first.c_str()))
                 m_CurrentTest = test.second();
