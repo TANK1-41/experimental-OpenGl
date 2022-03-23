e30e5ba8bfbbbc15b218dc8741301e5b668bfca5
TANK1_41 <bradenaey1@gmail.com>
2022-03-23 11:37:36 -0400

    test menu

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -24,3 +24,4 @@ void Renderer::draw(const VertexArray &va, const IndexBuffer &ib, const Shader &
     ib.Bind();
     GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
 }
+
