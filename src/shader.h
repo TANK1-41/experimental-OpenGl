771c12a24d2f98d61c2e0c239ac7fee5ee5fecc8
TANK1_41 <bradenaey1@gmail.com>
2022-03-29 18:30:45 -0400

    completed shader class

--- src/shader.h
+++ src/shader.h
@@ -5,26 +5,28 @@
 #pragma once
 
 #include "GL/glew.h"
-#include <xstring>
 #include <fstream>
-#include <sstream>
 #include <iostream>
+#include <sstream>
+#include <xstring>
 
 class shader {
 public:
     //the program ID assigned by open gl
-        unsigned int ID;
+    unsigned int ID;
 
     //constructor reads and builds the shader
-    shader(const char* vertexPath, const char* fragmentPath);
+    shader(const char *vertexPath, const char *fragmentPath);
+    ~shader();
     //activiate / bind the shader
     void use();
+
     //utility uniform functions
-    void setBool(const std::string& name, bool value) const;
-    void setInt(const std::string& name, int value) const;
-    void setFloat(const std::string& name, float value) const;
+    void setBool(const std::string &name, bool value) const;
 
+    void setInt(const std::string &name, int value) const;
+
+    void setFloat(const std::string &name, float value) const;
+
+    void setVec4(const std::string &name, float v1,float v2,float v3,float v4) const;
 };
-
-
-
